223b25029408f40d701988361c4c631194190a7b
Samuel <adeoyesamuel100@gmail.com>
2025-11-26 13:09:06 +0100

    making update

--- app.py
+++ app.py
@@ -78,6 +78,6 @@ def predict():
 
 # Run the Flask server
 # commented out because of vercel deployment
-#if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host="0.0.0.0", port=port, debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
